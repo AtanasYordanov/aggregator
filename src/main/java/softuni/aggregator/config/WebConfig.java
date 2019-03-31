501f6d9c10e0486451f7d5037e1beaf12dade54a
Atanas <naskoraist@gmail.com>
2019-03-31 14:45:11 +0300

    Styling. Added another interceptor.

--- src/main/java/softuni/aggregator/config/WebConfig.java
+++ src/main/java/softuni/aggregator/config/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import softuni.aggregator.web.interceptor.PerformanceInterceptor;
+import softuni.aggregator.web.interceptor.UsernameInterceptor;
 
 @Configuration
 @EnableSpringDataWebSupport
@@ -14,5 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new PerformanceInterceptor())
                 .addPathPatterns("/imports/**", "/exports/**");
+
+        registry.addInterceptor(new UsernameInterceptor());
     }
 }
