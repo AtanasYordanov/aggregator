b8253b40ca605e8d58720ec2d3034e8884c4f583
Atanas <naskoraist@gmail.com>
2019-04-12 20:35:38 +0300

    Adjustment.

--- src/main/java/softuni/aggregator/service/excel/constants/ExcelConstants.java
+++ src/main/java/softuni/aggregator/service/excel/constants/ExcelConstants.java
@@ -1,7 +1,7 @@
 package softuni.aggregator.service.excel.constants;
 
 public final class ExcelConstants {
-    public static final String EXPORT_BASE_PATH = "exports\\";
+    public static final String EXPORT_BASE_PATH = "\\exports\\";
     public static final String EXPORT_FILE_EXTENSION = ".xlsx";
 
     private ExcelConstants() {
